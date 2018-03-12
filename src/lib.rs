e3e8c8573e1d09a152f8d43b6068179da5fe5e2c
Nico Grunbaum <na-g@nostrum.com>
2018-03-12 12:58:03 -0700

    Use JSON strings instead of custom wrappers for everything.

--- src/lib.rs
+++ src/lib.rs
@@ -1,85 +1,50 @@
 #![feature(proc_macro)]
 
+extern crate serde;
+extern crate serde_json;
 extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
 use std::fmt;
-use std::rc::Rc;
 
 extern crate rsdparsa;
 use rsdparsa::error::SdpParserError;
 use rsdparsa::SdpSession;
+use serde::ser::Serialize;
 
-#[wasm_bindgen]
-#[derive(Clone)]
-pub struct JrSdpParserError { error: Rc<SdpParserError> }
-
-impl From<SdpParserError> for JrSdpParserError {
-    fn from(error:SdpParserError) -> Self {
-        JrSdpParserError { error: Rc::new(error) }
-    }
+trait Jsonable {
+    fn to_json_string(&self) -> String;
 }
 
-impl fmt::Debug for JrSdpParserError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.error)
-    }
-}
-
-#[wasm_bindgen]
-impl JrSdpParserError {
-    fn get_line_number(&self) -> String {
-        match *self.error {
-            SdpParserError::Line{ref line_number, ..} => line_number,
-            SdpParserError::Unsupported{ref line_number, ..} => line_number,
-            SdpParserError::Sequence{ref line_number, ..} => line_number,
-        }.to_string()
-    }
-}
-
-#[wasm_bindgen]
-#[derive(Clone)]
-pub struct JrSdpSession { session: Rc<SdpSession> }
-
-impl From<SdpSession> for JrSdpSession {
-    fn from(session:SdpSession) -> Self {
-        JrSdpSession { session: Rc::new(session) }
-    }
-}
-
-impl fmt::Debug for JrSdpSession {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Session {}", self.session.get_session())
-    }
-}
-
-#[wasm_bindgen]
-impl JrSdpSession {
-    pub fn version(&self) -> String {
-        return self.session.get_version().to_string()
+impl<T:Serialize> Jsonable for T {
+    fn to_json_string(&self) -> String {
+        match serde_json::to_string(&self) {
+            Err(_) => "".to_string(),
+            Ok(json_string) => json_string,
+        }
     }
 }
 
 #[wasm_bindgen]
 pub struct JrParseResult {
-    result: Result<JrSdpSession, JrSdpParserError>
+    result: String,
+    is_err: bool
 }
 
 impl From<Result<SdpSession, SdpParserError>> for JrParseResult {
     fn from(result:Result<SdpSession, SdpParserError>) -> Self {
-        JrParseResult { result: match result {
-                Err(error) => Err(JrSdpParserError::from(error)),
-                Ok(session) => Ok(JrSdpSession::from(session)),
-            }
+        JrParseResult {
+            result: match result {
+                Err(ref error) => error.to_json_string(),
+                Ok(ref session) => session.to_json_string()
+            },
+            is_err: result.is_err(),
         }
     }
 }
 
 impl fmt::Debug for JrParseResult {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match &self.result {
-            &Err(ref err) => write!(f, "ParseResult {:?}", err),
-            &Ok(ref session) => write!(f, "Parse Result {:?}", session),
-        }
+        write!(f, "ParseResult {}", self.result.to_json_string())
     }
 }
 
@@ -87,16 +52,10 @@ impl fmt::Debug for JrParseResult {
 #[wasm_bindgen]
 impl JrParseResult {
     pub fn is_err(&self) -> bool {
-        self.result.is_err()
+        self.is_err
     }
-    pub fn is_ok(&self) -> bool {
-        self.result.is_ok()
-    }
-    pub fn err(&self) -> JrSdpParserError {
-        self.result.as_ref().unwrap_err().clone()
-    }
-    pub fn ok(&self) -> JrSdpSession {
-        self.result.as_ref().unwrap().clone()
+    pub fn result(&self) -> String {
+        self.result.clone()
     }
 }
 
